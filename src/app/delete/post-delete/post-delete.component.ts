6c763c0341494ee32eefdb9190aaebeb4368b67b
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-10-07 10:50:36 -0300

    Implementação de PUT e DELETE de Post

--- src/app/delete/post-delete/post-delete.component.ts
+++ src/app/delete/post-delete/post-delete.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Post } from 'src/app/model/Post';
+import { Theme } from 'src/app/model/Theme';
+import { PostService } from 'src/app/service/post.service';
+import { ThemeService } from 'src/app/service/theme.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-post-delete',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDeleteComponent implements OnInit {
 
-  constructor() { }
+  post: Post = new Post();
+  idPost: number;
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private postService: PostService
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0, 0)
+
+    if (environment.token == '') {
+      this.router.navigate(['/login']);
+    }
+
+    this.idPost = this.activatedRoute.snapshot.params['idPost'];
+    this.findByIdPost(this.idPost);
   }
+
+  findByIdPost(idPost: number) {
+    this.postService.getByIdPost(idPost).subscribe((resp: Post) => {
+      this.post = resp;
+    })
+  }
+ 
+  toDelete() {
+    this.postService.deletePost(this.idPost).subscribe(() => {
+      alert('Postagem deletada com sucesso!');
+      this.router.navigate(['/inicio']);
+    })
+  }
+
 
 }
